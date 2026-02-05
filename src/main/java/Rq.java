bbdb45babec1efce49bec7865871c8e05309116b
hansol0118 <wall197944@naver.com>
2026-02-05 14:36:51 +0900

    12강 -> t9 변경, - b

--- src/main/java/Rq.java
+++ src/main/java/Rq.java
@@ -31,7 +31,11 @@ public class Rq {
 
     public int getParamAsInt(String key, int defaultValue) {
         String rst = getParam(key);
-        return Integer.parseInt(rst);
+        try {
+            return Integer.parseInt(rst);
+        } catch (NumberFormatException e) {
+            return defaultValue;
+        }
     }
 }
 
